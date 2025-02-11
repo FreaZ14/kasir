d2669563cb3ada2218fc5c9a283ee989579b786e
Farhan <syauqifarhan211@gmail.com>
2025-02-11 13:01:29 +0700

    4th commit

--- resources/views/livewire/user.blade.php
+++ resources/views/livewire/user.blade.php
@@ -1,3 +1,28 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-12">
+                <button wire:click="pilihMenu('lihat')" 
+                    class="btn {{ $pilihanMenu=='lihat' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    Semua Pengguna
+                </button>
+                <button wire:click="pilihMenu('tambah')" 
+                    class="btn {{ $pilihanMenu=='tambah' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    Tambah Pengguna
+                </button>
+                <button wire:loading class="btn btn-info">
+                    Loading....
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+            @if($pilihanMenu=='lihat')
+            @elseif($pilihanMenu=='tambah')
+            @elseif($pilihanMenu=='edit')
+            @elseif($pilihanMenu=='hapus')
+            @endif
+            </div>
+    </div>
 </div>
